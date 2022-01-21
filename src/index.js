2c0a7d8dab90688a58fc8bd29be3a576e29e880f
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-01-21 16:27:04 -0500

    add/ useSatate, add,substrac & reset buttons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
-import {App} from "./App.js";
 import './index.css';
+import { CounterApp } from "./CounterApp";
 
 const root = document.querySelector("#root");
-
-ReactDom.render(<App />,root);
+ReactDom.render(<CounterApp value={10}/>,root);
   
